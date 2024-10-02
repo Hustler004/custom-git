39dcf24eee7b7063608dff79956a6c5b375c563e
Hustler004 <agamagarwal004@gmail.com>
2024-10-02 12:18:51 +0530

    added read-blob functionality

--- app/main.js
+++ app/main.js
@@ -1,15 +1,18 @@
 const fs = require("fs");
 const path = require("path");
+const zlib = require("zlib");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
+// console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const command = process.argv[2];
 
 switch (command) {
   case "init":
     createGitDirectory();
+    break;
+  case "cat-file":
+    readBlob();
     break;
   default:
     throw new Error(`Unknown command ${command}`);
@@ -27,4 +30,28 @@ function createGitDirectory() {
     "ref: refs/heads/main\n"
   );
   console.log("Initialized git directory");
+}
+
+function readBlob() {
+  try {
+    const hash = process.argv[4];
+    const sub_dir = hash.substring(0, 2);
+    const filename = hash.substring(2);
+    const content = fs.readFileSync(
+      path.join(process.cwd(), `.git/objects/${sub_dir + "/" + filename}`)
+      // "utf-8"
+    );
+
+    const decompressed = zlib.inflateSync(content);
+    const nullCharIndex = decompressed.indexOf(0);
+
+    // Extract the actual content after the null character
+    const actualContent = decompressed.slice(nullCharIndex + 1);
+
+    // Convert the content to a string (assuming utf-8 encoding)
+    const decompressedString = actualContent.toString("utf-8");
+    process.stdout.write(decompressedString);
+  } catch (err) {
+    console.log(err);
+  }
 }
